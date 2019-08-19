e61d6b11ba7f0fe69d665083890fe561c7ad8ebd
Amit-Nativ <amitnativ11@gmail.com>
2019-08-19 09:40:27 +0300

    feat(group-by): added groupby option
    used reduce in order to split into groups. initilal value is {} (an empty object)

---
+++ index.js
@@ -0,0 +1,18 @@
+const heroes = require('./Exercise 3/ow');
+
+const names = [...heroes.names];
+const roles = [...heroes.roles];
+const hp = [...heroes.hp];
+
+const getHeroes = () => names.map((x,i) => ({name: x, role: roles[i]}));
+
+const groupBy = (arr, key='role') => arr.reduce((result, x) => {
+    let group = x[key];
+    result[group] = result[group] || [];
+    result[group].push(x);
+
+    return result;
+}, {});
+
+
+console.log(groupBy(getHeroes()));
